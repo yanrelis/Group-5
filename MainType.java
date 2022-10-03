7783770881b83b0949458d059b9adc50bcaf25e3
Matthew Lardi <102744327+1matthewsteven8@users.noreply.github.com>
2022-10-03 11:34:38 -0400

    Add files via upload

---
+++ MainType.java
@@ -0,0 +1,8 @@
+package jframe;
+
+public class MainType {
+	 public static void main(String[] args) {  
+		// SystemFrame systemFrame = new SystemFrame();
+		 WelcomePage welcomePage = new WelcomePage();
+	 }
+}
