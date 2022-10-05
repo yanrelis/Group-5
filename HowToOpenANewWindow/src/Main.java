d7fe964f29bc9af5a6e2883f44d59ee747beb5f4
yanrelis <79792187+yanrelis@users.noreply.github.com>
2022-10-04 21:41:24 -0400

    Update Main.java

--- HowToOpenANewWindow/src/Main.java
+++ HowToOpenANewWindow/src/Main.java
@@ -1,4 +1,8 @@
-
+/* Project: Project Milestone 2: GUI
+Class: Main.java
+Author: Yanelis Cruceta
+Date: October 2, 2022
+This class allows a you to be able to open a new window from one frame. This is the main page. */
 public class Main {
 
 	public static void main(String[] args) {
