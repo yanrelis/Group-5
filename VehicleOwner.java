75af477ad90a34bf988e7a8dbb3c7ccc3a99cd60
andrew-vargas <andrew.vargas19@stjohns.edu>
2022-10-23 06:45:15 -0400

    I implemented the generateOwnerID() method.
    A random ownerID between 1000 and 9999 will be randomly generated. If the ID has already been used, it will generate a new ID and repeat the process. Once a unique ID has been generated, the method will return it.

--- VehicleOwner.java
+++ VehicleOwner.java
@@ -5,6 +5,7 @@
 * This class is used to create objects pertaining to each individual vehicle owner.
 */ 
 
+import java.io.*;
 import java.util.*;
 
 public class VehicleOwner extends User {
@@ -24,11 +25,39 @@ public class VehicleOwner extends User {
 		ownerID = generateOwnerID();
 	}
 	
-	public static int generateOwnerID()
+	public static int generateOwnerID() // This method generates a random ID that hasn't been used yet.
 	{
-		int generatedID = (int) (Math.random() * 10000);
+		int generatedID = (int) (1000 + (Math.random() * 8999));
 		
-		// I still need to finish the code that ensures the randomly generated ID hasn't already been used.
+		try
+		{
+			File file = new File("SavedInfo\\OwnerInfo.txt");
+			file.getParentFile().mkdirs();
+			Scanner scnr = new Scanner(file);
+			System.out.println(generatedID);
+			
+			while (scnr.hasNext())
+			{
+				String currentLine = scnr.nextLine();
+				
+				if ((currentLine.length() > 8) && (currentLine.substring(0, 8).equals("OwnerID:")))
+				{
+					int usedID = Integer.parseInt(currentLine.substring(9, currentLine.length()));
+					if (generatedID == usedID)
+					{
+						System.out.println("Match found!");
+						generateOwnerID();
+					}
+				}
+			}
+			
+			System.out.println("No match found!");
+			scnr.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("File cannot be found.");
+		}
 		
 		return generatedID;
 	}
@@ -56,6 +85,11 @@ public class VehicleOwner extends User {
 	public void addVehicle(Vehicle vehicle)
 	{
 		ownedVehicles.add(vehicle);
+	}
+	
+	public static void main(String[] args)
+	{
+		generateOwnerID();
 	}
 	
 }
