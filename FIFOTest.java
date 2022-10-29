cd028090f6f60d23ce990719960c4b9b01a3b750
andrew-vargas <andrew.vargas19@stjohns.edu>
2022-10-29 00:34:37 -0400

    I created the getCompletionTime method.

--- FIFOTest.java
+++ FIFOTest.java
@@ -25,21 +25,38 @@ public class FIFOTest {
 		return queue.element();
 	}
 	
+	
+	/* The method below calculates the amount of time it will take for the job entered to be completed (including the duration
+	of the job entered). */
+	public int getCompletionTime(Job job)
+	{
+		int time = job.getJobDuration();
+		Queue<Job> tempqueue = queue;
+		Job currentJob = tempqueue.remove();
+		
+		while (currentJob.getJobID() != job.getJobID())
+		{
+			time += currentJob.getJobDuration();
+			currentJob = tempqueue.remove();
+		}
+		
+		return time;
+	}
+	
 	public static void main(String[] args)
 	{
 		Job test = new Job(5, 5, "A", "B");
 		Job test2 = new Job(10, 10, "C", "D");
+		Job test3 = new Job(20, 20, "C", "D");
+		Job test4 = new Job(30, 30, "C", "D");
 		
 		FIFOTest tester = new FIFOTest();
 		tester.queueAdd(test);
 		tester.queueAdd(test2);
+		tester.queueAdd(test3);
+		tester.queueAdd(test4);
 		
-		Job currentJob = tester.getCurrentJob();
-		System.out.println(currentJob.getJobDuration());
-		tester.queueRemove();
-		
-		currentJob = tester.getCurrentJob();
-		System.out.println(currentJob.getJobDuration());
+		System.out.println(tester.getCompletionTime(test4));
 	}
 	
 }
