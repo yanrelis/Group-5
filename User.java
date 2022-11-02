555a664e7c4f81f255352ba5e3c8bd4bbe83559b
Matthew Lardi <102744327+1matthewsteven8@users.noreply.github.com>
2022-11-02 12:25:47 -0400

    Update User.java

--- User.java
+++ User.java
@@ -5,51 +5,49 @@
 * This abstract class contains basic information regarding users and methods to obtain and change them.
 */ 
 
-public abstract class User {
-	
+public class User {
+
 	private String firstName;
 	private String lastName;
 	private String phoneNumber;
 	private String email;
-	
-	public String getFirstName()
-	{
+
+	public String getFirstName() {
 		return firstName;
 	}
-	
-	public void setFirstName(String firstName)
-	{
+
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
-	public String getLastName()
-	{
+
+	public String getLastName() {
 		return lastName;
 	}
-	
-	public void setLastName(String lastName)
-	{
+
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	public String getPhoneNumber()
-	{
+
+	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	
-	public void setPhoneNumber(String phoneNumber)
-	{
+
+	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
-	public String getEmail()
-	{
+
+	public String getEmail() {
 		return email;
 	}
-	
-	public void setEmail(String email)
-	{
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
+	public String toString() {
+		String output = "\n First Name: " + firstName + "\n Last Name: " + lastName + "\n Phone Number: " + phoneNumber
+				+ "\n Email: " + email + "\n Username: ";
+		return output;
+	}
+
 }
